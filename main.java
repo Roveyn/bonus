e6564baae1c1616c0e24bcc1c49662b949452f03
Rita Oveyan <rita_oveyan@mail.ru>
2022-06-30 20:17:21 +0300

    first commit

---
+++ main.java
@@ -0,0 +1,20 @@
+public class main {
+
+    public static void main(String[] args) {
+        int amount = 100;
+        int x = 1100;
+
+        boolean isField = true;
+
+        //int ruble = isField ? 1 : 0;
+        int ruble;
+        if (isField) {
+            ruble = 1;
+        } else {
+            ruble = 0;
+        }
+
+        int bonus = x / 100 * ruble;
+        System.out.println("Сумма бонуса: " + bonus);
+    }
+}
